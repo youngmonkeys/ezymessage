88a7935a68d00743893a8590e667119243603bf6
conght11 <conght11>
2022-02-17 22:31:42 +0700

    fix code style

--- src/main/java/com/ezy/databasesetting/repository/DatabaseListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ezy.databasesetting.repository;
-
-import com.ezy.databasesetting.entitiy.DatabaseList;
-import com.tvd12.ezydata.mongodb.EzyMongoRepository;
-import com.tvd12.ezyfox.database.annotation.EzyRepository;
-
-@EzyRepository
-public interface DatabaseListRepository extends EzyMongoRepository<Long,DatabaseList> {
-}
